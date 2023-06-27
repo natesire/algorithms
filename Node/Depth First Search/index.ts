4cbcb10e391616a946bc2c523c5a2d3946ffca3e
Nate Sire <nate.sire@fairwaymc.com>
2023-06-27 17:31:10 -0400

    DFS can be more decoupled from the recursive function call

--- Node/Depth First Search/index.ts
+++ Node/Depth First Search/index.ts
@@ -1,20 +1,20 @@
-import { TreeNode } from "./TreeNode";
+import { Vertex } from "./GraphNode";
 
-let node1 = new TreeNode(1);
-let node2 = new TreeNode(2);
-let node3 = new TreeNode(3);
+let node1 = new Vertex(1);
+let node2 = new Vertex(2);
+let node3 = new Vertex(3);
 
-node1.leftEdge = node2;
-node1.rightEdge = node3;
+node1.leftChildVertex = node2;
+node1.rightChildVertex = node3;
 
-let node4 = new TreeNode(4);
-let node5 = new TreeNode(5);
-let node6 = new TreeNode(6);
-let node7 = new TreeNode(7);
+let node4 = new Vertex(4);
+let node5 = new Vertex(5);
+let node6 = new Vertex(6);
+let node7 = new Vertex(7);
 
-node2.leftEdge = node4;
-node2.rightEdge = node5;
-node3.leftEdge = node6;
-node3.rightEdge = node7;
+node2.leftChildVertex = node4;
+node2.rightChildVertex = node5;
+node3.leftChildVertex = node6;
+node3.rightChildVertex = node7;
 
 node1.search(7);
