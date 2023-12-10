f437318878c86e96645022d50a4462448d9b2895
Nate Sire <natesire@gmail.com>
2023-12-10 13:04:25 -0500

    print out all built in functions

---
+++ Python3/all_functions.py
@@ -0,0 +1,40 @@
+import math
+import string
+
+i = 1
+print("int")
+print(dir(i))
+print("\n")
+
+f = 1.1
+print("float")
+print(dir(f))
+print("\n")
+
+print("math")
+print(dir(math))
+print("\n")
+
+print("string")
+print(dir(string))
+print("\n")
+
+list_var = [1, 2, 3]
+print("list")
+print(dir(list_var))
+print("\n")
+
+set = {1,2,3,4,5,6,7,8,9,10}
+print("set")
+print(dir(set))
+print("\n")
+
+dict_var = {'key': 'value'}
+print("dictionary")
+print(dir(dict_var))
+print("\n")
+
+bool = True
+print("bool:")
+print(dir(bool))
+print("\n")
--- Python3/bubble_sort.py
+++ Python3/bubble_sort.py
@@ -1,4 +1,29 @@
-# must define function before calling it unlike in NodeJS
+class BubbleSort:
+    def __init__(self, arr):
+        self.arr = arr;
+    
+    def sort(self):
+        sorted = self.arr;
+    
+        # for each element in the array
+        for i in range(len(sorted)):
+            # for each element in the array
+            for j in range(len(sorted)):
+                # < will produce ascending, > will produce descending
+                if sorted[i] > sorted[j]:
+                    # swap the elements
+                    temp = sorted[i];
+                    sorted[i] = sorted[j];
+                    sorted[j] = temp;
+    
+        return sorted;
+
+    def swap(self, i1, i2):
+        temp1 = self.arr[i1];
+        temp2 = self.arr[i2];
+        self.arr[i1] = temp2;
+        self.arr[i2] = temp1;
+
 def bubble_sort(arr):
     sorted = arr;
 
