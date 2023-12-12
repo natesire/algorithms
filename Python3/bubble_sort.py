b4e0ab32c3b23fceb75adf35b5aef9623d12c3a6
Nate Sire <nate.sire@fairwaymc.com>
2023-12-12 15:46:05 -0500

    bubble sort works

---
+++ Python3/bubble_sort.py
@@ -0,0 +1,18 @@
+# must define function before calling it unlike in NodeJS
+def bubble_sort(arr):
+    sorted = arr;
+
+    # for each element in the array
+    for i in range(len(sorted)):
+        # for each element in the array
+        for j in range(len(sorted)):
+            # if the element is greater than the next element
+            if sorted[i] < sorted[j]:
+                # swap the elements
+                temp = sorted[i];
+                sorted[i] = sorted[j];
+                sorted[j] = temp;
+
+    return sorted;
+
+print(bubble_sort([1,0,3,2,5,4]))
