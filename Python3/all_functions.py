558a3313e3ca3a499e882a6ba94113a16aed46df
Nate Sire <nate.sire@fairwaymc.com>
2023-12-13 17:14:00 -0500

    refactor names

--- Python3/all_functions.py
+++ Python3/all_functions.py
@@ -1,19 +1,22 @@
-# straight forward way to analyze all the built-in functions and methods for various types
+# built-in functions are different from class methods but can be read the same way
+# all class methods are functions but functions are not necessarily methods themselves
 
 import math
 import string
+import time
 
-def print_methods(target):
-    methods_or_functions = dir(target)
-    count_methods = len(methods_or_functions)
-    # get type of target
-    target_type = type(target)
-    print(f'{count_methods} {target_type} methods or functions {methods_or_functions}\n')
+def print_methods(module_or_type):
+    methods_or_functions = dir(module_or_type)
+    count_functions = len(methods_or_functions)
+    target_type = type(module_or_type)
+    target_name = target_type.__name__
+    print(f'{count_functions} {target_type} {target_name}, methods or functions: {methods_or_functions}\n')
 
 print_methods(1)
 print_methods(1.1)
 print_methods(math)
 print_methods(string)
+print_methods(time)
 print_methods([1,2,3])
 print_methods({1,2,3})
 print_methods({'key': 'value'})
