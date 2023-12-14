889c4bec3b8466ebfa109fb4c279ac7b60dc8788
Nate Sire <nate.sire@fairwaymc.com>
2023-12-13 22:55:52 -0500

    refactor types into list

--- Python3/all_functions.py
+++ Python3/all_functions.py
@@ -1,28 +1,21 @@
 # built-in functions are different from class methods but can be read the same way
 # all class methods are functions but functions are not necessarily methods themselves
 
-import math
-import string
-import time
 
 def print_methods(module_or_type):
     methods_or_functions = dir(module_or_type)
     count_functions = len(methods_or_functions)
     target_type = type(module_or_type)
     target_name = target_type.__name__
-    print(f'{count_functions} {target_type} {target_name}, methods or functions: {methods_or_functions}\n')
+    print(f'{count_functions} {target_type} {target_name}')
+    for method in methods_or_functions:
+        print(f'{method}()')
 
-print_methods(1)
-print_methods(1.1)
-print_methods(math)
-print_methods(string)
-print_methods(time)
-print_methods([1,2,3])
-print_methods({1,2,3})
-print_methods({'key': 'value'})
-print_methods(True)
+types = [1, 1.1, "str", [1,2,3], {1,2,3}, {'key': 'value'}, True]
+for t in types:
+    print_methods(t)
 
-# check if these match exactly
 # all the built in methods for Python are returned in a dictionary
 print_methods(__builtins__)
 print(dir(__builtins__))
+
